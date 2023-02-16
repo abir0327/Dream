b88e25eb787911dc9f6cabdb1e5e0b0be427f779
abir0327 <125345704+abir0327@users.noreply.github.com>
2023-02-16 15:11:38 +0600

    Add files via upload

---
+++ Models/ApplicationDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dreamblood.Models
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Books> Book { get; set; }
+    }
+    
+}
